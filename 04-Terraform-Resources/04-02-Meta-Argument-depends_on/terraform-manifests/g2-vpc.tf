1b5da86a8fe26dce55181af078083f379f899aa5
grk05 <grkds05@gmail.com>
2022-01-19 17:19:36 +0530

    --- Auto-Git Commit ---

--- 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/g2-vpc.tf
+++ 04-Terraform-Resources/04-02-Meta-Argument-depends_on/terraform-manifests/g2-vpc.tf
@@ -8,22 +8,68 @@ resource    "aws-vpc" "dev_vpc" {
    }
 }
  # Resource-2: Create Subnets
-resource "aws_subnet" "vpc_dev_public_subnet-1" {
+resource "aws_subnet" "vpc-dev-public-subnet-1" {
    vpc_id = aws_vpc.vpc-dev.id
    cidr_block ="10.0.1.0/24"
    avaavailability_zone = "us-east-1a"
-   map_publimap_public_ip_on_launch = true+ 
+   map_publimap_public_ip_on_launch = true 
+  }
   
+# Resource-3: Internet Gateway
+resource "aws_internet_gateway" "vpc-dev-igw" {
+   vpc_vpc_id = aws_vpc.vpc_dev.id
+     
+}
+# Resource-4: Create Route Table
+resource "aws_route_table" "vpc-dev-public-route-table" {
+   vpc_vpc_id = aws_vpc.vpc_dev.id
+    
 }
 
-# Resource-3: Internet Gateway
-
-# Resource-4: Create Route Table
-
-
 # Resource-5: Create Route in Route Table for Internet Access
-
+resource "aws_route" "vpc-dev-public-route" {
+   route_table_id = aws_route_table.vpc-dev-public-route-table.id 
+   destination_cidr_block = "0.0.0.0/0"
+   gateway_id = aws_internet_gateway.vpc-dev-igw.id
+}
 
 # Resource-6: Associate the Route Table with the Subnet
+resource "aws_route_table_association" "vpc-dev-public-route-table-associate" {
+   rouroute_table_id = aws_route_table.vpc-dev-public-subnet-1.id
+   subnet_subnet_id = aws_subnet.vpc-dev-public-subnet-1.id
+     
+}
 
 # Resource-7: Create Security Group
+resource "aws_security_group" "dec-vpc-sg" {
+   name = "dev-vpc-default-sg"
+   vpc_vpc_id = aws_vpc.vpc-dev.id
+   description = "Dev VPC Default Security Group"
+
+   ingress {
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Allow Port 80"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description = "Allow all ip and ports outboun"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}  
+  
+}
